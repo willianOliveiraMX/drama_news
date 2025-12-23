ad8d23497d882b088f49a66599e24927bb5ae65b
Willian Alves <willian.alves@luizalabs.com>
2025-12-22 21:54:58 -0300

    chore: initial commit

---
+++ src/app/artigos/[type]/[slug]/page.js
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <div>
+      <h1>Isso é um artigo</h1>
+    </div>
+  )
+};
+
+export default Page;
---
+++ src/app/artigos/page.js
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <div>
+      <h1>Isso aqui são os artigos!</h1>
+    </div>
+  );
+};
+
+export default Page;
